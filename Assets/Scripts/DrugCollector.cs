2ce99712e438890d48d8b73942e7b99561617b59
PrakritiNayak <prakritinayak7@gmail.com>
2023-10-31 13:57:27 +0000

    First aesthetics

---
+++ Assets/Scripts/DrugCollector.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrugCollector : MonoBehaviour
+{
+    private int drugs = 0;
+    
+
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+      if (collision.gameObject.CompareTag("Drug"))
+      {
+        Destroy(collision.gameObject);
+        drugs++;
+        Debug.Log("Drugs: " + drugs);
+       
+      }
+        
+    }
+}
