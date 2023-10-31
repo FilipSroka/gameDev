03bb1aec638691da0c8cb0dbc8d88c2d5ab63d51
Connor Watts <connorwatts@hotmail.com>
2023-10-31 10:47:02 +0000

    feat/drug-effects

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -1,24 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform player;
-    public float smoothing = 5f;
-    public Vector2 offset;
+    [SerializeField] private Transform player;
 
-    void Update()
+    private void Update()
     {
-        // Ensure we have a player reference
-        if (player == null)
-            return;
-
-        // Calculate the target position based on the player's position
-        Vector2 targetPosition = new Vector2(player.position.x, transform.position.y) + offset;
-
-        // Smoothly move the camera towards the target position
-        Vector2 smoothedPosition = Vector2.Lerp(transform.position, targetPosition, smoothing * Time.deltaTime);
-
-        // Assign the new position using a Vector3
-        transform.position = new Vector3(smoothedPosition.x, smoothedPosition.y, transform.position.z);
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z); //vector 3 because camera is at a z value to be able to see everything, unline the rest
     }
 }
