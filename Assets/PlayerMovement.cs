1f0f990637aba5a022993fa37266167f4c98768c
Filip Sroka <filipsroka@filips-air.wkstn.qmul.ac.uk>
2023-11-01 14:47:53 +0000

    Fixed water killing

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -106,7 +106,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("WaterDrop"))
+        if (collision.gameObject.CompareTag("WaterDrop") || collision.gameObject.CompareTag("Water"))
         {
             float distanceTraveled = transform.position.x - startingPoint.x;
             if (distanceTraveled > maxDistance)
